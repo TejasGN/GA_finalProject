e814de0a6b1d5a736332e292ec9c35c15a9ef045
TejasN <tj.tejas77@gmail.com>
2022-02-17 02:56:25 -0500

    updates and project build

--- portfolio/app_public/portfolio-public/src/pages/fourth/fourth.js
+++ portfolio/app_public/portfolio-public/src/pages/fourth/fourth.js
@@ -10,15 +10,15 @@ class Fourth extends React.Component{
                     <div className="proj-wrap">
                         <div className="proj">
                             <h4>PDT Website</h4>
-                            <a href="#" target="_blank"><img src="images/pdt-website.jpg" alt="pdt-website" /></a>
+                            <a href="https://pdts-projct.000webhostapp.com/" target="_blank"><img src="images/pdt-website.jpg" alt="pdt-website" /></a>
                         </div>
                         <div className="proj">
                             <h4>Utility Dashboard</h4>
-                            <a href="#" target="_blank"><img src="images/dashboard.jpg" alt="dashboard" /></a>
+                            <a href="https://js-dashboard.000webhostapp.com/" target="_blank"><img src="images/dashboard.jpg" alt="dashboard" /></a>
                         </div>
                         <div className="proj">
                             <h4>Image Gallery</h4>
-                            <a href="#" target="_blank"><img src="images/img-gallry.jpg" alt="image-gallery" /></a>
+                            <a href="https://img-app.000webhostapp.com/" target="_blank"><img src="images/img-gallry.jpg" alt="image-gallery" /></a>
                         </div>
                         <div className="proj">
                             <h4>Find The Penguins Game</h4>
